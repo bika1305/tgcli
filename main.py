ae315cd75f456a1ced3aa03d35af742ef2e1ea9b
bika1305 <iwan.vlehanov@gmail.com>
2025-06-26 12:04:56 +0700

    Up

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 from telethon import TelegramClient
+from utils.config import API_ID, API_HASH
+from utils.messages import send_message
+import asyncio
+
+client = TelegramClient('session_name', API_ID, API_HASH)
+
+async def main():
+    # Подключение
+    await client.start()
+
+    # Отправка тестового сообщения
+    await send_message(client, 'me', 'test')
+
+    
+    await client.disconnect()
+
+if __name__ == "__main__":
+    asyncio.run(main())
